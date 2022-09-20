ea5dfe8bab9296c072b99e8f86d7f30b17766abb
tyler77bradford1993@gmail.com <tyler77bradford1993@gmail.com>
2022-09-20 02:08:20 +0000

    Developing stage orders

---
+++ models/Staging/stg_orders.sql
@@ -0,0 +1,19 @@
+SELECT 
+    A.orderID
+    ,A.orderdate
+    ,A.shipdate
+    ,A.shipmode
+    ,A.ordersellingprice - A.Ordercostprice AS OrderProfit
+    ,A.Ordercostprice
+    ,A.ordersellingprice
+    ,B.customername
+    ,B.segment
+    ,B.country
+    ,C.category
+    ,C.productname
+    ,c.subcategory
+FROM {{ ref('raw_orders') }} AS A
+    LEFT JOIN {{ ref('raw_customer') }} AS B
+        ON A.CustomerID = B.CustomerID
+    LEFT JOIN {{ ref('raw_product') }} AS C
+        ON A.ProductID  = C.ProductID
